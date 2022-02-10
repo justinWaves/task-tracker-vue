51f4791d07f2e79a9d4c05424459fe5efa95e767
justinWaves <juhstinn@gmail.com>
2022-02-10 01:21:23 -0800

    ui constructed

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
   <div id="app">
-    <h1>hello world</h1>
+    <div class="container">
+      <Header
+        @toggle-add-task="toggleAddTask"
+        title="Task Tracker"
+        :showAddTask="showAddTask"
+      />
+      <div v-show="showAddTask"><AddTask @add-task="addTask" /></div>
+
+      <Tasks
+        @toggle-reminder="toggleReminder"
+        @delete-task="deleteTask"
+        :tasks="tasks"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
+import AddTask from "./AddTask.vue";
+
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  } 
-}
+    Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+
+    deleteTask(id) {
+      if (confirm("Are you sure?")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 20th at 2:00pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Birthday Laser Tag",
+        day: "Feb 22th at 2:22pm",
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: "Yoga Class",
+        day: "April 14th at 4:55pm",
+        reminder: true,
+      },
+    ];
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
   max-width: 500px;
